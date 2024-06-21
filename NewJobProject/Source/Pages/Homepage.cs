4d5197e7419731b17f3764c804fae4090f2c317a
srikanth munna <sikanthmunna@outlook.com>
2024-06-21 14:13:58 +0100

    Add project files.

---
+++ NewJobProject/Source/Pages/Homepage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewJobProject.Source.Pages
+{
+    public class Homepage
+    {
+        
+        private IWebDriver _driver;
+        private IWebElement SearchBox => _driver.FindElement(By.Id("twotabsearchtextbox"));
+        private IWebElement SearchButton => _driver.FindElement(By.Id("nav-search-submit-button"));
+        private IWebElement GroceryButton => _driver.FindElement(By.Id("nav-link-groceries"));
+
+
+
+        public Homepage(IWebDriver driver)
+        {
+            _driver = driver;
+
+        }
+
+
+        public void EnterSerachBox(string data)
+        {
+            SearchBox.SendKeys(data);
+
+        }
+
+        public void ClickSearchButton() => SearchButton.Click();
+
+        public void ClickGroceryButton()
+        {
+            Thread.Sleep(1000);
+            GroceryButton.Click();
+        }
+
+    }
+}
---
+++ NewJobProject/Test/HomeTest.cs
@@ -0,0 +1,54 @@
+﻿using NewJobProject.Source.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NewJobProject.Test
+{
+
+
+    public class HomeTest
+    {
+
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitiSetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("https:www.amazon.co.uk");
+        }
+
+
+        [Test]
+        public void Serchpage()
+        {
+            Homepage _homepage = new Homepage(_driver);
+            //hp.EnterSerachBox("Greeting Cards");
+            _homepage.ClickGroceryButton();
+            Assert.True(_driver.Title.Contains("Groceries"));
+
+
+        }
+
+        [Test]
+        public void SearchForProduct()
+        {
+
+            
+
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+
+            _driver.Dispose();
+        }
+
+    }
+}
